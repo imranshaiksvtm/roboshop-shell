a0beda6019679b557722aaed5758743b0475f8a6
imranshaiksvtm <imranshaiksvtm013@gmail.com>
2023-02-01 16:59:26 +0530

    mogodb script

--- mongodb.sh
+++ mongodb.sh
@@ -1,8 +1,8 @@
 script_location=$(pwd)
 
-cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo
+cp ${script_location}/files/mongodb.repo /etc/yum.repo.d/mongodb.repo
 
-yum install mongodb-org -y
+yum yum install mongodb-org -y
 
 systemctl enable mongod
 systemctl start mongod
