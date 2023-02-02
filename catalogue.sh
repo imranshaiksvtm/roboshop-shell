4c2d095be5e7285c288006d7dbe24bbf759c4ba7
imranshaiksvtm <imranshaiksvtm013@gmail.com>
2023-02-02 16:47:19 +0530

    catalogue script

--- catalogue.sh
+++ catalogue.sh
@@ -1,6 +1,6 @@
 
 script_location=$(pwd)
-set -e
+
 
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 yum install nodejs -y
@@ -17,3 +17,6 @@ cp ${script_location}/files/catalogue.service /etc/systemd/system/catalogue.serv
 systemctl daemon-reload
 ststemctl enable catalogue
 systemctl start catalogue
+
+cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo
+yum install mongodb-org-shell -y
